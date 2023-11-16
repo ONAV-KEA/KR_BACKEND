a52c5cfd77741511df738fbbf4dfc51edf2e61d2
Teller501 <ande777j@stud.kea.dk>
2023-11-16 15:07:31 +0100

    initial commit

---
+++ src/main/java/dk/kea/onav2ndproject_rest/Onav2ndProjectRestApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.onav2ndproject_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onav2ndProjectRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Onav2ndProjectRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/onav2ndproject_rest/Onav2ndProjectRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.onav2ndproject_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onav2ndProjectRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
