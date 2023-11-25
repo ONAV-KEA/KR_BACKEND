d4ab5bd40e0b3618d653b81c9c81bfdc3c559425
Teller501 <ande777j@stud.kea.dk>
2023-11-25 17:57:49 +0100

    updated event and user relation with middle table UserEventDetails

---
+++ src/main/java/dk/kea/onav2ndproject_rest/entity/UserEventDetails.java
@@ -0,0 +1,27 @@
+package dk.kea.onav2ndproject_rest.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class UserEventDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Boolean participating;
+    private String additionalNotes;
+}
