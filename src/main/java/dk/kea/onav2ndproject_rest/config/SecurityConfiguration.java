8916655fc6af8715ddd887fd434e34de2b2f2d79
Teller501 <ande777j@stud.kea.dk>
2023-12-11 13:43:55 +0100

    cross allow all patterns

--- src/main/java/dk/kea/onav2ndproject_rest/config/SecurityConfiguration.java
+++ src/main/java/dk/kea/onav2ndproject_rest/config/SecurityConfiguration.java
@@ -64,7 +64,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println("addCorsMappings called");
         registry.addMapping("/**")  // /** means match any string recursively
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*/") //Multiple strings allowed. Wildcard * matches all port numbers.
+                .allowedOriginPatterns("*") //Multiple strings allowed. Wildcard * matches all port numbers.
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // decide which methods to allow
                 .allowCredentials(true);
     }
